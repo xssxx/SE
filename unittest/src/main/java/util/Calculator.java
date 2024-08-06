923eb13352b6f4711883dd15db329e52010f0cb9
X <koonx6520@gmail.com>
2024-08-06 11:22:19 +0700

    Add files via upload

---
+++ unittest/src/main/java/util/Calculator.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Calculator {
+    public int power(int left, int right) {
+        // Raises left to the power of right
+        // Return -1 if right < 0
+        // Ex: power(3,2) = 9
+        int result = 1;
+        if (right < 0)
+            return -1;
+        for (int i = 1; i <= right; i++)
+            result = result * left;
+        return result;
+    }
+}
---
+++ unittest/src/test/java/util/CalculatorTest.java
@@ -0,0 +1,53 @@
+package util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @BeforeEach
+    void setup() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testPowerBaseZeroPowerNegative() {
+        assertThrows(ArithmeticException.class, () -> calculator.power(0, -1));
+    }
+
+    @Test
+    void testPowerBaseZeroPowerZero() {
+        assertEquals(1, calculator.power(0, 0));
+    }
+    @Test
+    void testPowerBasePositivePowerZero() {
+        assertEquals(1, calculator.power(5,0));
+    }
+
+    @Test
+    void testPowerBasePositivePowerOne() {
+        assertEquals(5, calculator.power(5,1));
+    }
+
+    @Test
+    void testPowerBaseNegativePowerZero() {
+        assertEquals(1, calculator.power(-5, 0));
+    }
+
+    @Test
+    void testPowerBaseNegativePowerOne() {
+        assertEquals(-5, calculator.power(-5, 1));
+    }
+
+    @Test
+    void testPowerBasePositivePowerNegative() {
+        assertEquals(0.2, calculator.power(5, -1));
+    }
+
+    @Test
+    void testPowerBaseNegativePowerNegative() {
+        assertEquals(-0.2, calculator.power(-5, -1));
+    }
+}
