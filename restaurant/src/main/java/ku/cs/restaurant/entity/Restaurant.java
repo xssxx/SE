43be789d05ae8e93d84c2dfaac86a6480c109983
X <koonx6520@gmail.com>
2024-08-20 20:15:34 +0700

    Add files via upload

---
+++ restaurant/src/main/java/ku/cs/restaurant/entity/Restaurant.java
@@ -0,0 +1,51 @@
+package ku.cs.restaurant.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+public class Restaurant {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private double rating;
+    private String location;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
